8c36a3d3f63176a7bf123c45d16a3fdc075337ae
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2020-06-10 14:42:51 +0200

    Projekat iz Računarske grafike

---
+++ ebejtovic1_RGProjekat/Assets/ebejtovic1_script.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ebejtovic1_script : MonoBehaviour {
+    Ray ray;
+    RaycastHit hit;
+
+    public GameObject videoPlane;
+    public GameObject videoPOI;
+
+    public GameObject slikaPlane;
+    public GameObject slikaPOI;
+
+    public GameObject tekst;
+    public GameObject tekstPOI;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider.gameObject.name== "ebejtovic1_videoPOI")
+            {
+                videoPOI.SetActive(false);
+                videoPlane.SetActive(true);   
+            }
+
+            if (hit.collider.gameObject.name == "ebejtovic1_planeVideo")
+            {
+                videoPlane.SetActive(false);
+                videoPOI.SetActive(true);
+            }
+
+            if (hit.collider.gameObject.name == "ebejtovic1_planeSlika")
+            {
+                slikaPlane.SetActive(false);
+                slikaPOI.SetActive(true);
+            }
+
+            if (hit.collider.gameObject.name == "ebejtovic1_slikaPOI")
+            {
+                slikaPlane.SetActive(true);
+                slikaPOI.SetActive(false);
+            }
+
+            if (hit.collider.gameObject.name == "ebejtovic1_tekstPOI")
+            {
+                tekstPOI.SetActive(false);
+                tekst.SetActive(true);
+                
+            }
+
+            if (hit.collider.gameObject.name == "ebejtovic1_tekst")
+            {
+                tekst.SetActive(false);
+                tekstPOI.SetActive(true);
+            }
+
+
+        }
+		
+	}
+
+   
+}
---
+++ ebejtovic1_RGProjekat/Assets/ebejtovic1_skripta2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ebejtovic1_skripta2 : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject infoTekst;
+    public GameObject helpTekst;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void funkcijaOnClick()
+    {
+        Application.Quit();
+    }
+
+    public void funkcijaInfo()
+    {
+        if (infoTekst.activeInHierarchy == true)
+        {
+            infoTekst.SetActive(false);
+        }
+        else { 
+            infoTekst.SetActive(true);
+        }
+    }
+
+    public void funkcijaHelp()
+    {
+        if (helpTekst.activeInHierarchy == true)
+        {
+            helpTekst.SetActive(false);
+        }
+        else
+        {
+            helpTekst.SetActive(true);
+        }
+    }
+}
